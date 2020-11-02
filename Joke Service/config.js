99186fbc050d91eb0c604395cabd37115dd30c9e
Rasmus Hansen <rasmushansen97@gmail.com>
2020-11-02 11:14:54 +0100

    bliv gjort i stand

--- Joke Service/config.js
+++
@@ -1,7 +0,0 @@
-// config.js
-const config = {
-    databaseURI: 'mongodb+srv://rbhansen:VCu7h40iwaJkTE1J@cluster0.aq1xn.mongodb.net/Jokes_DB?retryWrites=true&w=majority', // or MongoDB Atlas connection URI
-    localPort: 8080
-};
-
-module.exports = config;
---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+const config = {
+    databaseURI:' mongodb+srv://rbhansen:VCu7h40iwaJkTE1J@cluster0.aq1xn.mongodb.net/Jokes_DB?retryWrites=true&w=majority', //'mongodb+srv://jbmoe:karsten36@cluster0.tvzjb.mongodb.net/Jokes_DB?retryWrites=true&w=majority', // or MongoDB Atlas connection URI
+    localPort: 8080
+};
+
+module.exports = config;
